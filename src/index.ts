edacd351d093b8b6030d313854a44d25c6e9528e
Nicu Eremia <nicolae.eremia@codefactorygroup.com>
2018-08-02 17:41:14 +0300

    prom-async

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,3 @@ import { map, filter, sample } from 'rxjs/operators';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
-
-/* const observable = new Observable((observer) => {
-  observer.next(5);
-});
-
-observable.subscribe(value => console.log(value + '!'));
-
-console.log('And now we are here.'); */
